e6935b02d76cc9a46fc5a46daaa833c10bc20d9c
metehan876161-oss <metehan876161@gmail.com>
2025-08-03 22:09:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,26 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hesap Makinesi API Çalışıyor"
-
-@app.route('/hesapla', methods=['POST'])
-def hesapla():
+@app.route('/<sayi1><islem><sayi2>')
+def hesapla(sayi1, islem, sayi2):
     try:
-        data = request.get_json()
-        num1 = float(data['num1'])
-        num2 = float(data['num2'])
-        choice = data['choice']
-
-        if choice == '+':
-            result = num1 + num2
-        elif choice == '-':
-            result = num1 - num2
-        elif choice == '*':
-            result = num1 * num2
-        elif choice == '/':
-            if num2 == 0:
-                return jsonify({'error': 'Sıfıra bölünemez'})
-            result = num1 / num2
+        sayi1 = float(sayi1)
+        sayi2 = float(sayi2)
+        if islem == '+':
+            return str(sayi1 + sayi2)
+        elif islem == '-':
+            return str(sayi1 - sayi2)
+        elif islem == '*':
+            return str(sayi1 * sayi2)
+        elif islem == '/':
+            if sayi2 == 0:
+                return "Sıfıra bölünemez!"
+            return str(sayi1 / sayi2)
         else:
-            return jsonify({'error': 'Geçersiz işlem'})
+            return "Geçersiz işlem!"
+    except:
+        return "Hata oluştu!"
 
-        return jsonify({'sonuç': result})
-    except Exception as e:
-        return jsonify({'error': str(e)})
+if __name__ == '__main__':
+    app.run()
